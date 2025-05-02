589aa81943fa67e7acea3612259f004040ea0fc8
Oskar Kotyla <oskarkotyla123@gmail.com>
2025-05-02 11:51:34 +0200

    Implemented better looking favourite cards

--- src/main/resources/static/js/favourites.js
+++ src/main/resources/static/js/favourites.js
@@ -1,4 +1,4 @@
-// File: /src/main/resources/static/js/my-page.js
+// File: `IDATA2301_12/src/main/resources/static/js/favourites.js`
 document.addEventListener("DOMContentLoaded", function () {
     console.log("Starting fetch for /favourites/user");
     fetch("/favourites/user")
@@ -8,14 +8,21 @@ document.addEventListener("DOMContentLoaded", function () {
             }
             return response.json();
         })
-        .then(bookings => {
-            console.log("Favourites received:", bookings);
-            const tmpl = document.getElementById("tmpl-booking-card");
-            const listEl = document.getElementById("bookings-list");
-            bookings.forEach(b => {
+        .then(favourites => {
+            console.log("Favourites received:", favourites);
+            const tmpl = document.getElementById("tmpl-favourites-card");
+            const listEl = document.getElementById("favourites-list");
+            favourites.forEach(f => {
                 const clone = document.importNode(tmpl.content, true);
-                clone.querySelector(".fv-hotel").textContent    = f.id;
-                clone.querySelector(".fc-lc-type").textContent = f.listing.hotel.name;
+                const hotelNameEl = clone.querySelector(".fv-hotel-name");
+                const locTypeEl = clone.querySelector(".fv-lc-type");
+                const cityEl = clone.querySelector(".fv-lc-city");
+                const countryEl = clone.querySelector(".fv-lc-country");
+
+                if (hotelNameEl) hotelNameEl.textContent = f.listing.hotel.name;
+                if (locTypeEl) locTypeEl.textContent = f.listing.hotel.roomTypes;
+                if (cityEl) cityEl.textContent = f.listing.hotel.city;
+                if (countryEl) countryEl.textContent = f.listing.hotel.country;
                 listEl.appendChild(clone);
             });
         })

32d85cdef777492b7522b45d6785de1fb5156559
Oskar Kotyla <oskarkotyla123@gmail.com>
2025-05-02 10:26:08 +0200

    Implemented uch of the login, mypage, and favourites pages

--- src/main/resources/static/js/favourites.js
+++ src/main/resources/static/js/favourites.js
@@ -1,7 +1,7 @@
 // File: /src/main/resources/static/js/my-page.js
 document.addEventListener("DOMContentLoaded", function () {
-    console.log("Starting fetch for /bookings/user");
-    fetch("/bookings/user")
+    console.log("Starting fetch for /favourites/user");
+    fetch("/favourites/user")
         .then(response => {
             if (!response.ok) {
                 throw new Error("Network response was not OK: " + response.status);
@@ -9,16 +9,13 @@ document.addEventListener("DOMContentLoaded", function () {
             return response.json();
         })
         .then(bookings => {
-            console.log("Bookings received:", bookings);
+            console.log("Favourites received:", bookings);
             const tmpl = document.getElementById("tmpl-booking-card");
             const listEl = document.getElementById("bookings-list");
             bookings.forEach(b => {
                 const clone = document.importNode(tmpl.content, true);
-                clone.querySelector(".bk-id").textContent    = b.id;
-                clone.querySelector(".bk-hotel").textContent = b.listing.hotel.name;
-                clone.querySelector(".bk-start").textContent = b.startDate;
-                clone.querySelector(".bk-end").textContent   = b.endDate;
-                clone.querySelector(".bk-status").textContent= b.status;
+                clone.querySelector(".fv-hotel").textContent    = f.id;
+                clone.querySelector(".fc-lc-type").textContent = f.listing.hotel.name;
                 listEl.appendChild(clone);
             });
         })

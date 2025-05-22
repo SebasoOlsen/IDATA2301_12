eed839934cf5f7a51cc6e187b557993c089efa70
Jonas <jonaolse@stud.ntnu.no>
2025-05-22 16:47:46 +0200

    Refactor product page and add css

--- src/components/Product/AvailableListingsBox.jsx
+++ src/components/Product/AvailableListingsBox.jsx
@@ -1,23 +1,33 @@
 import React from "react";
+import "../../assets/css/available-listings.css"
 
 export default function AvailableListingsBox({ listings }) {
   const { room, provider, price, currency } = listings;
   return (
-    <div className="available-listings-box">
-      <h3>Available Listings</h3>
+      <div className="wrapper">
+          <h3 className="title">Available Listings</h3>
+        <div className="available-listings-box">
       {listings.length === 0 ? (
-        <p>No available listings found.</p>
+        <p className="not-found-message">No available listings found.</p>
       ) : (
         listings.map((listing, idx) => (
-          <div key={idx} className="listing-item">
-            <h4>{listing.room.name}</h4>
-            <p>{listing.provider.name}</p>
-            <p>
-              <strong>Price:</strong> {listing.price} {listing.currency}
-            </p>
-          </div>
+            <div className="listing-item-wrapper">
+                <div key={idx} className="listing-item">
+                    <div className="room-info">
+                        <h4 className ="room-type">{listing.room.name}</h4>
+                        <p className="provider-name">{listing.provider.name}</p>
+                    </div>
+                </div>
+                <div className= "price-info">
+                    <strong className="price-label"> Price: </strong>
+                    <p className="cost-p">{listing.price} </p>
+                    <p className="currency-p">{listing.currency} </p>
+                </div>
+                <button className="view-dates-button">View dates</button>
+            </div>
         ))
       )}
-    </div>
+        </div>
+      </div>
   );
 }
